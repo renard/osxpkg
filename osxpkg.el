c4ff9138bf72167eca101a0fd879ac0eb6788582
Sébastien Gross <sebchezwamorg>
2012-10-29 20:39:56 +0100

    Set buffer writable before modifs.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- osxpkg.el
+++ osxpkg.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2012-10-18
-;; Last changed: 2012-10-29 20:19:00
+;; Last changed: 2012-10-29 20:39:23
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
